ce8ff4da8580d713468c7e0de1be198f320b5ece
Максим Брейлян <maxim.breilyan@yandex.ru>
2024-09-12 19:36:30 +0300

    добавил @Size

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,9 @@ public class ItemDto {
 
     private int id;
     @NotBlank
+    @Size(min = 1, max = 50, message = "Максимальная длина имени — 50 символов.")
     private String name;
+    @Size(min = 1, max = 255, message = "Максимальная длина описания — 255 символов.")
     @NotBlank(message = "Описание не может быть пустым")
     private String description;
     @NotNull
